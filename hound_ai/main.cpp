316a058a72e26f6e6ba43e516053a8e0041fdf47
apolitowicz <politowicz205@gmail.com>
2017-08-12 16:26:34 -0500

    overhauled file name structure, added usage of included file functions, and added functionality for reading from shared memory

--- hound_ai/main.cpp
+++ hound_ai/main.cpp
@@ -1,5 +1,5 @@
-#include <Windows.h>
-#include <cstdio>
+#include "main.h"
+#include <iostream>
 
 //LPCWSTR file_location = TEXT("C:\\Users\\ap\\Documents\\Games\\Emulators\\dolphin-master-5.0-321-x64\\Dolphin-x64\\Dolphin.exe");
 wchar_t create_proc_command[256] = TEXT("C:\\Users\\ap\\Documents\\Games\\Emulators\\dolphin-master-5.0-321-x64\\Dolphin-x64\\Dolphin.exe -e \"C:\\Users\\ap\\Documents\\Games\\Emulators\\ROMs\\Super Smash Bros. Melee (v1.02).iso\"");
@@ -33,20 +33,43 @@ bool mainSetup()
 
 int main()
 {
-	printf("Welcome to Hound_AI\n");
-	printf("Now setting up program and target\n\n");
+	std::wcout << L"Welcome to Hound_AI" << std::endl;
+	std::wcout << L"Now setting up program and target\n" << std::endl;
 	
 	//setup
 	bool setup_complete = mainSetup();
 	if (!setup_complete) {
-		printf("Failed Dolphin setup with error code: %d\n", GetLastError());
+		DWORD err = GetLastError();
+		std::wcout << L"Failed Dolphin setup with error code: " << err;
+		std::wstring err_msg = parseWinError(err);
+		std::wcout << " " << err_msg << std::endl;
 		return -1;
 	}
 	DWORD pid = pi.dwProcessId;
 
-	printf("Dolphin PID: %ld\n", pid);
+	std::wcout << L"Dolphin PID: " << pid << std::endl;
 
-	//inject DLL and access mapped file
+	//inject DLL
+	std::wcout << L"Now injecting Hound_DLL into Dolphin" << std::endl;
+
+	PROCESSENTRY32 pe32 = { sizeof(PROCESSENTRY32) };
+	HANDLE hproc_snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	setupInject(L"hound_DLL.dll", L"Dolphin.exe", pid, pe32, hproc_snap);
+
+	//access mapped file
+	std::wstring mapfile_name = L"Global\\HoundAIMap";
+	HANDLE hmapfile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, mapfile_name.c_str());
+	if (hmapfile == NULL) {
+		std::wcout << L"Failed opening mapped file: " << parseWinError(GetLastError()) << std::endl;
+		return -1;
+	}
+	LPCWSTR mapbuf = (LPCWSTR) MapViewOfFile(hmapfile, FILE_MAP_ALL_ACCESS, 0, 0, 256);
+	if (mapbuf == NULL) {
+		std::wcout << L"Failed accessing view of mapped file: " << parseWinError(GetLastError()) << std::endl;
+		return -1;
+	}
+
+	//setup named pipes
 
 
 	//TODO: continue
--- hound_ai/main.h
+++ hound_ai/main.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#ifndef _HOUND_AI_H_
+#define _HOUND_AI_H_
+
+#include <Windows.h>
+#include "DLL_injector_lib.h"
+#include "Win_err_parse_lib.h"
+
+#endif
